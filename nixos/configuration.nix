d5db0ce615510859cefc2dc2cd705ee7d127a7eb
k-kahora <mango22mjk@gmail.com>
2023-11-26 13:57:26 +0000

    Enablin hyprland

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -116,6 +116,7 @@
       ];
     };
   };
+  progams.hyprland.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
