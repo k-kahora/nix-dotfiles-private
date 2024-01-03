c4a546b54a8d34d71006c10e7004080fe402b089
k-kahora <mango22mjk@gmail.com>
2024-01-03 01:57:36 +0000

    comic code ligatures commited

--- nixos/comic-code.nix
+++ nixos/comic-code.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out/share/fonts/opentype
-    cp $src $out/share/fonts/opentype/${pname}.otf
+    cp -r $src $out/share/fonts/opentype/${pname}.otf
   '';
 
   meta = with lib; {
