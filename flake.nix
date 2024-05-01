47b4a83c9925c61056b0d8dc25fbc99a71338b42
k-kahora <mango22mjk@gmail.com>
2024-05-01 21:50:57 +0000

    nix version 2.17

--- flake.nix
+++ flake.nix
@@ -9,13 +9,13 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
-    nixvim = {
-      url = "github:nix-community/nixvim";
+    #nixvim = {
+      #url = "github:nix-community/nixvim";
       # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
       # url = "github:nix-community/nixvim/nixos-23.05";
 
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
+      #inputs.nixpkgs.follows = "nixpkgs-unstable";
+    #};
 
     nur.url = "github:nix-community/NUR";
 
